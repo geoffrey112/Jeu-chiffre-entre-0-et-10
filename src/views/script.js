89a5c3c39b1bb402c2f0364c07474320b0f66cbc
geoffrey112 <geoffrey.legall@orange.fr>
2020-10-30 23:47:55 +0100

    Add life-content

---
+++ src/views/script.js
@@ -0,0 +1,10 @@
+// If difficult Easy, the random number are defined 1 more time
+// If difficult Hard, the random number are defined every round
+
+//If number are inside, enabled "Ok" button 
+
+// If number of chance.. display life
+// Re-boot game with button
+
+
+
