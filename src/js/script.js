aeec6c833cdd82e6416025b75f38b633fdb9bbeb
geoffrey112 <geoffrey.legall@orange.fr>
2020-12-04 01:50:54 +0100

    Add select life

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,31 @@
 
 //If number are inside, enabled "Ok" button 
 
-// If number of chance.. display life
 // Re-boot game with button
+
+// Between 0-10 only and no charac
+
+
+
+
+let select = document.getElementById("selectChance");
+
+select.addEventListener('change', function(){
+  select = document.getElementById("selectChance").value;
+
+  if(select == 2){
+    document.getElementById("life2").style.visibility = "visible";
+    document.getElementById("life1").style.visibility = "hidden";
+  }else if(select == 3){
+    document.getElementById("life2").style.visibility = "visible";
+    document.getElementById("life1").style.visibility = "visible";
+  }else{
+    document.getElementById("life2").style.visibility = "hidden";
+    document.getElementById("life1").style.visibility = "hidden";
+  }
+  
+});
+
+
+
 
