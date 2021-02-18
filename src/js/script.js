f18228dec39b7d733e2ab3604d47c705cae301a4
geoffrey112 <geoffrey.legall@orange.fr>
2021-02-18 20:30:54 +0100

    Start class

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,47 @@
-// Difficult: Display color
-let difficult = document.getElementById("difficulty");
 
-difficult.addEventListener('change', function(){
-  difficult = document.getElementById("difficulty").value;
-  
-  if(difficult === "easy"){
-    document.getElementById("difficulty").style.backgroundColor = "#04f804";
-  }else{
-    document.getElementById("difficulty").style.backgroundColor = "red";
+class Display{
+  constructor(difficulty){
+    this._difficulty = difficulty;
   }
+
+  get difficulty(){  
+    
+    if(this._difficulty === "easy"){
+      // document.getElementById("difficulty").style.backgroundColor = "#04f804";
+      console.log('test ok');
+    }else{
+      // document.getElementById("difficulty").style.backgroundColor = "red";
+      console.log('test non');
+    }
+  }
+
+}
+
+let displayColor = new Display(document.getElementById('difficulty').value);
+
+// Add changement listener ici
+
+console.log(displayColor.difficulty);
+
+
+
+
+
+
+
+// Difficult: Display color
+// let difficult = document.getElementById("difficulty");
+
+// difficult.addEventListener('change', function(){
+//   difficult = document.getElementById("difficulty").value;
   
-});
+//   if(difficult === "easy"){
+//     document.getElementById("difficulty").style.backgroundColor = "#04f804";
+//   }else{
+//     document.getElementById("difficulty").style.backgroundColor = "red";
+//   }
+  
+// });
 
 
 
@@ -46,9 +77,6 @@ selectChance.addEventListener('change', function(){
 let randomNb = Math.floor(Math.random()* 11);
 let nbOfChance = parseInt(document.getElementById("selectChance").value);
 let submit = document.getElementById("ok");
-let enter = document.getElementById("findNb");
-
-console.log(randomNb); //CHECK
 
 submit.addEventListener('click', function(){
   
@@ -90,7 +118,6 @@ submit.addEventListener('click', function(){
         document.getElementById("difficulty").disabled = true;
         document.getElementById("selectChance").disabled = true;
         --nbOfChance;
-        console.log(randomNb); //CHECK
       }else if(nb !== randomNb && nbOfChance === 2){
         document.getElementById("answer").innerHTML = "Ouch, try again.. Last chance";
         document.getElementById("life2").style.visibility = "hidden";
@@ -98,7 +125,6 @@ submit.addEventListener('click', function(){
         document.getElementById("difficulty").disabled = true;
         document.getElementById("selectChance").disabled = true;
         --nbOfChance;
-        console.log(randomNb); //CHECK
       }else{
         document.getElementById("life3").style.visibility = "hidden";
         document.getElementById("answer").innerHTML = "Perdu!";
@@ -107,22 +133,6 @@ submit.addEventListener('click', function(){
     }
   }
 
-});
-
-
-
-// Enabled Ok
-enter.addEventListener('input', function(){
-
-  enter = document.getElementById("findNb").value;
-
-  if(enter > 10 || enter < 0){
-    document.getElementById("ok").disabled = true;
-  }else if(enter == "" || enter == "-" || enter == "e"){
-    document.getElementById("ok").disabled = true;
-  }else{
-    document.getElementById("ok").disabled = false;
-  }
 });
 
 
@@ -145,15 +155,31 @@ reset.addEventListener('click', function(){
   document.getElementById("findNb").value = "";
   nbOfChance = 1;
   randomNb = Math.floor(Math.random()* 11);
-  console.log(`Après reset ${randomNb}`); //CHECK
 });
 
 
-// Enable keyboard Enter
+
+// Enabled Ok
+let enter = document.getElementById("findNb");
+
+enter.addEventListener('input', function(){
+
+  enter = document.getElementById("findNb").value;
+
+  if(enter > 10 || enter < 0){
+    document.getElementById("ok").disabled = true;
+  }else if(enter == "" || enter == "-" || enter == "e"){
+    document.getElementById("ok").disabled = true;
+  }else{
+    document.getElementById("ok").disabled = false;
+  }
+});
+
+
+
+// Enable Enter button
 enter.addEventListener('keydown', function(e){
   if(e.key === "Enter"){
     submit.click();
   }
 });
-
-
