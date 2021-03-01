bd23fc2bc819073b80e79d174b6d5dfe759cca2b
geoffrey112 <geoffrey.legall@orange.fr>
2021-03-01 12:53:08 +0100

    update game class

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 // Display Nb of life - Color - Ok button
 class Display{
-  constructor(difficulty, life, enableOk){
+  constructor(difficulty, nbOfLife, enableOk){
     this._difficulty = difficulty;
-    this._life = life;
+    this._nbOfLife = nbOfLife;
     this._enableOk = enableOk;
   }
 
@@ -19,11 +19,11 @@ class Display{
     this._difficulty = value;
   }
 
-  get life(){
-    return this._life;
+  get nbOfLife(){
+    return this._nbOfLife;
   }
 
-  set life(value){
+  set nbOfLife(value){
     if(value == 2){
       document.getElementById("life2").style.visibility = "visible";
       document.getElementById("life1").style.visibility = "hidden";
@@ -37,7 +37,7 @@ class Display{
       document.getElementById("life1").style.visibility = "hidden";
       document.getElementById("answer").innerHTML = "";
     }
-    this._life = value;
+    this._nbOfLife = value;
   }
 
   get enableOk(){
@@ -64,15 +64,83 @@ let displayElement = new Display('easy', 1);
 document.getElementById('difficulty').addEventListener('change', () => {
   displayElement.difficulty = document.getElementById('difficulty').value;
 });
-
 // Nb of life
 document.getElementById('selectChance').addEventListener('change', () => {
-  displayElement.life = document.getElementById('selectChance').value;
+  displayElement.nbOfLife = document.getElementById('selectChance').value;
 });
-
 // Ok button
 document.getElementById('findNb').addEventListener('input', () => {
   displayElement.enableOk = document.getElementById('findNb').value;
+});
+
+
+
+// Game
+class Game extends Display{
+  constructor(difficulty, nbOfLife, randomNb, functionality, reset){
+    super(difficulty, nbOfLife);
+    this._randomNb = randomNb;
+    this._functionality = functionality;
+    this._reset = reset;
+  }
+
+  get randomNb(){
+    return this._randomNb;
+  }
+
+  set randomNb(value){
+    this._randomNb = value;
+  }
+
+  get functionality(){
+    return this._functionality;
+  }
+
+  set functionality(value){
+    if(value === this._randomNb && this._difficulty === 'easy'){
+      console.log("Test random OK");
+      // Prend la valeur hard aussi
+    }
+    
+  }
+
+  get reset(){
+    return this._reset;
+  }
+
+  set reset(value){
+    if(value){
+      document.getElementById("difficulty").style.backgroundColor = "#04f804";
+      document.getElementById("difficulty").value = "easy";
+      document.getElementById("life2").style.visibility = "hidden";
+      document.getElementById("life1").style.visibility = "hidden";
+      document.getElementById("life3").style.visibility = "visible";
+      document.getElementById("answer").innerHTML = "";
+      document.getElementById("difficulty").disabled = false;
+      document.getElementById("selectChance").disabled = false;
+      document.getElementById("selectChance").value = 1;
+      document.getElementById("ok").disabled = true;
+      document.getElementById("findNb").value = "";
+      this._randomNb = Math.floor(Math.random()*11);
+      // Remettre nbOfLife à 1 après reset
+      console.log("Après reset: " + this._randomNb);
+    }
+  }
+
+}
+
+let game = new Game('easy');
+game.randomNb = Math.floor(Math.random()*11);
+console.log("Random " + game.randomNb);
+
+// game process
+document.getElementById('ok').addEventListener('click', () => {
+  game.functionality = parseInt(document.getElementById('findNb').value);
+});
+
+// Reset
+document.getElementById('reset').addEventListener('click', () => {
+  game.reset = document.getElementById('reset');
 });
 
 
@@ -103,131 +171,67 @@ document.getElementById('findNb').addEventListener('keydown', (e) => {
 
 
 
-
 // Game
-class Game extends Display{
-  constructor(difficulty, life, randomNb, reset){
-    super(difficulty, life);
-    this._randomNb = randomNb;
-    this._reset = reset;
-  }
+// let randomNb = Math.floor(Math.random()* 11);
+// let nbOfChance = parseInt(document.getElementById("selectChance").value);
+// let submit = document.getElementById("ok");
 
-  get randomNb(){
-    return this._randomNb;
-  }
-
-  set randomNb(value){
-    this._randomNb = value;
-  }
-
-  get reset(){
-    return this._reset;
-  }
-
-  set reset(value){
-    if(value){
-      document.getElementById("difficulty").style.backgroundColor = "#04f804";
-      document.getElementById("difficulty").value = "easy";
-      document.getElementById("life2").style.visibility = "hidden";
-      document.getElementById("life1").style.visibility = "hidden";
-      document.getElementById("life3").style.visibility = "visible";
-      document.getElementById("answer").innerHTML = "";
-      document.getElementById("difficulty").disabled = false;
-      document.getElementById("selectChance").disabled = false;
-      document.getElementById("selectChance").value = 1;
-      document.getElementById("ok").disabled = true;
-      document.getElementById("findNb").value = "";
-      // nbOfChance = 1;
-      // randomNb = Math.floor(Math.random()* 11);
-      console.log("Après reset: " + randomNb);
-    }
-  }
-
-}
-
-let game = new Game();
-game.randomNb = Math.floor(Math.random()* 11);
-console.log("Random " + game.randomNb);
-// Remettre life à 1 après reset + check randomNb
-
-// Reset
-document.getElementById('reset').addEventListener('click', () => {
-  game.reset = document.getElementById('reset');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Game
-let randomNb = Math.floor(Math.random()* 11);
-let nbOfChance = parseInt(document.getElementById("selectChance").value);
-let submit = document.getElementById("ok");
-
-submit.addEventListener('click', function(){
+// submit.addEventListener('click', function(){
   
-  let nb = parseInt(document.getElementById("findNb").value);
-  let difficult = document.getElementById("difficulty").value;
+//   let nb = parseInt(document.getElementById("findNb").value);
+//   let difficult = document.getElementById("difficulty").value;
 
-  if(difficult == "easy"){ // Easy
-    if(nbOfChance > 0){
-      if(nb ===  randomNb){
-        document.getElementById("answer").innerHTML = "Gagnée!";
-        nbOfChance = 0;
-      }else if(nb != randomNb && nbOfChance === 3){
-        document.getElementById("life1").style.visibility = "hidden";
-        document.getElementById("answer").innerHTML = "Ouch, try again, 2 chance remaining";
-        document.getElementById("difficulty").disabled = true;
-        document.getElementById("selectChance").disabled = true;
-        --nbOfChance;
-      }else if(nb !== randomNb && nbOfChance === 2){
-        document.getElementById("answer").innerHTML = "Ouch, try again.. Last chance";
-        document.getElementById("life2").style.visibility = "hidden";
-        document.getElementById("difficulty").disabled = true;
-        document.getElementById("selectChance").disabled = true;
-        --nbOfChance;
-      }else{
-        document.getElementById("life3").style.visibility = "hidden";
-        document.getElementById("answer").innerHTML = "Perdu!";
-        --nbOfChance;
-      }
-    }
-  }else{ // Hard
-    if(nbOfChance > 0){
-      if(nb ===  randomNb){
-        document.getElementById("answer").innerHTML = "Gagnée!";
-        nbOfChance = 0;
-      }else if(nb != randomNb && nbOfChance === 3){
-        document.getElementById("life1").style.visibility = "hidden";
-        document.getElementById("answer").innerHTML = "Ouch, try again, 2 chance remaining";
-        randomNb = Math.floor(Math.random()* 11);
-        document.getElementById("difficulty").disabled = true;
-        document.getElementById("selectChance").disabled = true;
-        --nbOfChance;
-      }else if(nb !== randomNb && nbOfChance === 2){
-        document.getElementById("answer").innerHTML = "Ouch, try again.. Last chance";
-        document.getElementById("life2").style.visibility = "hidden";
-        randomNb = Math.floor(Math.random()* 11);
-        document.getElementById("difficulty").disabled = true;
-        document.getElementById("selectChance").disabled = true;
-        --nbOfChance;
-      }else{
-        document.getElementById("life3").style.visibility = "hidden";
-        document.getElementById("answer").innerHTML = "Perdu!";
-        --nbOfChance;
-      }  
-    }
-  }
+//   if(difficult == "easy"){ // Easy
+//     if(nbOfChance > 0){
+//       if(nb ===  randomNb){
+//         document.getElementById("answer").innerHTML = "Gagnée!";
+//         nbOfChance = 0;
+//       }else if(nb != randomNb && nbOfChance === 3){
+//         document.getElementById("life1").style.visibility = "hidden";
+//         document.getElementById("answer").innerHTML = "Ouch, try again, 2 chance remaining";
+//         document.getElementById("difficulty").disabled = true;
+//         document.getElementById("selectChance").disabled = true;
+//         --nbOfChance;
+//       }else if(nb !== randomNb && nbOfChance === 2){
+//         document.getElementById("answer").innerHTML = "Ouch, try again.. Last chance";
+//         document.getElementById("life2").style.visibility = "hidden";
+//         document.getElementById("difficulty").disabled = true;
+//         document.getElementById("selectChance").disabled = true;
+//         --nbOfChance;
+//       }else{
+//         document.getElementById("life3").style.visibility = "hidden";
+//         document.getElementById("answer").innerHTML = "Perdu!";
+//         --nbOfChance;
+//       }
+//     }
+//   }else{ // Hard
+//     if(nbOfChance > 0){
+//       if(nb ===  randomNb){
+//         document.getElementById("answer").innerHTML = "Gagnée!";
+//         nbOfChance = 0;
+//       }else if(nb != randomNb && nbOfChance === 3){
+//         document.getElementById("life1").style.visibility = "hidden";
+//         document.getElementById("answer").innerHTML = "Ouch, try again, 2 chance remaining";
+//         randomNb = Math.floor(Math.random()* 11);
+//         document.getElementById("difficulty").disabled = true;
+//         document.getElementById("selectChance").disabled = true;
+//         --nbOfChance;
+//       }else if(nb !== randomNb && nbOfChance === 2){
+//         document.getElementById("answer").innerHTML = "Ouch, try again.. Last chance";
+//         document.getElementById("life2").style.visibility = "hidden";
+//         randomNb = Math.floor(Math.random()* 11);
+//         document.getElementById("difficulty").disabled = true;
+//         document.getElementById("selectChance").disabled = true;
+//         --nbOfChance;
+//       }else{
+//         document.getElementById("life3").style.visibility = "hidden";
+//         document.getElementById("answer").innerHTML = "Perdu!";
+//         --nbOfChance;
+//       }  
+//     }
+//   }
 
-});
+// });
 
 
 
