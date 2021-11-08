0cc3f9c760f3719c23eef56dd21735c9dab1c759
geoffrey112 <geoffrey.legall@orange.fr>
2021-11-08 14:25:57 +0100

    update game

--- .old/script.js
+++ .old/script.js
@@ -1,20 +1,20 @@
 // Trouver le chiffre entre 0 et 10 avec 1 chance. OK
 
-let chiffre = 5;
-let joueur = prompt("Entrez un nombre entre 0 et 10");
+// let chiffre = 5;
+// let joueur = prompt("Entrez un nombre entre 0 et 10");
 
-if(joueur == chiffre){
-    alert("Bien joué! :)");
-}else{
-    alert("Perdu :(");
-}
+// if(joueur == chiffre){
+//     alert("Bien joué! :)");
+// }else{
+//     alert("Perdu :(");
+// }
 
 
 
 // Trouver le chiffre (statique) entre 0 et 10 avec 3 chance et les annoncer. OK
 // (Bonus) Prévenir si ce n'est pas entre 0 et 10. OK
 
-// let chiffre = Math.floor(Math.random() * 10);
+// let chiffre = Math.floor(Math.random() * 11);
 // console.log(chiffre); //(Verification chiffre statique)
 // let joueur = prompt("Choisi un chiffre entre 0 et 10");
 // let chance = 3;
@@ -41,26 +41,26 @@ if(joueur == chiffre){
 
 // De même, mais chiffre dynamique (aleatoire à chaque tour). OK
 
-// let chiffre = Math.floor(Math.random() * 10);
+// let chiffre = Math.floor(Math.random() * 11);
 // console.log(chiffre); //(Verification chiffre dynamique)
 // let joueur = prompt("Choisi un chiffre entre 0 et 10");
-// let chance = 2;
+// let chance = 3;
 
-// while(chance > -1){
+// while(chance > 0){
     
-//     if(chance > -1 && joueur > 10 || joueur < 0){
+//     if(joueur > 10 || joueur < 0){
 //         alert("Ce n'est pas entre 0 et 10 :/");
 //     }else if(joueur == chiffre){
 //         alert("Gagnée!");
 //         break;
-//     }else if(chance == 0 && joueur != chiffre){
+//     }else if(chance == 1 && joueur != chiffre){
 //         alert("Perdu :( " + "\nLe chiffre à trouver était: " + chiffre);
 //         break;
 //     }else{
-//         alert("Retente ta chance, il t'en reste: " + chance);
-//         --chance;
-//         chiffre = Math.floor(Math.random() * 10);
-//         console.log(chiffre); //(Verification chiffre dynamique)
+//       --chance;
+//       alert("Retente ta chance, il t'en reste: " + chance);
+//       chiffre = Math.floor(Math.random() * 11);
+//       console.log(chiffre); //(Verification chiffre dynamique)
 //     }
 
 //     joueur = prompt("Choisi un chiffre entre 0 et 10");
--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,182 @@
-// Ne pas mettre de logique dans Display, Game gère de pointer vers Diplay (voir sample.js)
-// Ne pas set-get si on ne change pas de valeur directement
-// Bien nommer nos variables/paramètres + créer des variable pour plusieurs document.getElement 
-
 class Display{
+  constructor(difficultElem, chanceElem, msg){
+    this.difficultElem = difficultElem;
+    this.chanceElem = chanceElem;
+    this.okButton = document.getElementById('ok');
+    this.msg = msg;
+    this.life1 = document.getElementById('life1');
+    this.life2 = document.getElementById('life2');
+    this.life3 = document.getElementById('life3');
+  }
 
-  // displayColor
-  // displayLife
-  // okStatus
-  // displayReset
-  // disabledSelect
+  init(){
+    this.displayColor();
+    this.displayLife();
+    this.okStatut(true);
+    this.disableSelect();
+    this.reset();
+  }
 
-  displayColor(lvl){
-    selectDifficulty = document.getElementById('difficulty');
+  displayColor(){
+    this.difficultElem = document.getElementById('difficulty');
 
-    if(lvl === 'hard'){
-      selectDifficulty.style.backgroundColor = "red";
-    }else{
-      selectDifficulty.style.backgroundColor = "#04f804";
+    this.difficultElem.addEventListener('change', () =>{
+      if(this.difficultElem.value == "hard"){
+        this.difficultElem.style.backgroundColor = "red";
+        this.messageMode("(The hard mode change the random number every turn)");
+      }else if(this.difficultElem.value == "easy"){
+        this.difficultElem.style.backgroundColor = "#04f804";
+        this.messageMode("");
+      }
+    });
+  }
+
+  displayLife(){
+    this.chanceElem = document.getElementById('selectChance');
+
+    this.chanceElem.addEventListener('change', () =>{ // Before game start (display life)
+      if(this.chanceElem.value == 2){
+        this.life1.style.visibility = "hidden";
+        this.life2.style.visibility = "visible";
+      }else if(this.chanceElem.value == 3){
+        this.life1.style.visibility = "visible";
+        this.life2.style.visibility = "visible";
+      }else if(this.chanceElem.value == 1){
+        this.life1.style.visibility = "hidden";
+        this.life2.style.visibility = "hidden";
+      }
+    });
+
+  }
+
+  looseLife(){
+    if(this.chanceElem.value == 2){
+      this.life1.style.visibility = 'hidden';
+    }else if(this.chanceElem.value == 1){
+      this.life2.style.visibility = 'hidden';
+    }else if(this.chanceElem.value == 0){
+      this.life3.style.visibility = 'hidden';
     }
+  }
+
+  okStatut(statut){
+    this.okButton.disabled = statut;
+  }
+
+  message(){
+    this.msg = document.getElementById('answer');
+
+    if(this.randomNum == this.input.value){
+      this.msg.innerHTML = "Win!";
+    }else if(this.chanceElem.value == 0){
+      this.msg.innerHTML = "Game over <img src='../../rick-sad.png' alt='Rick-sad' width='50'>";
+    }else if(this.chanceElem.value == 1){
+      this.msg.innerHTML = "Ouch, try again.. Last chance";
+    }else if(this.chanceElem.value == 2){
+      this.msg.innerHTML = "Ouch, try again, 2 chance remaining";
+    }
+
+  }
+
+  messageMode(msg){
+    const msgHardMode = document.getElementById('descHard');
+    msgHardMode.innerHTML = msg;
+  }
+
+  disableSelect(){
+    this.okButton.addEventListener('click', () =>{
+      this.difficultElem.disabled = true;
+      this.chanceElem.disabled = true;
+    });
+  }
+  
+  reset(){
+    let resetElem = document.getElementById('reset'); 
+    
+    resetElem.addEventListener('click', () =>{
+      location.reload();
+    });
+  }
+
+
+
+}
+
+class Game extends Display{
+  constructor(chanceElem, difficultElem, okButton, randomNum, input){
+    super(chanceElem, difficultElem, okButton);
+
+    this.chanceElem = document.getElementById('selectChance');
+    this.difficultElem = document.getElementById('difficulty');
+    this.randomNum = randomNum;
+    this.input = input;
+  }
+
+  init(){
+    this.randomNb();
+    this.enableOkButton();
+    this.process();
+  }
+  
+
+  randomNb(){
+    this.randomNum = Math.floor(Math.random()*11);
+    // console.log(this.randomNum);
+  }
+
+  enableOkButton(){
+    this.input = document.getElementById('findNb');
+
+    this.input.addEventListener('input', () =>{
+      if(this.input.value != "" && this.input.value <= 10 && this.input.value >= 0){
+        this.okStatut(false);
+      }else{
+        this.okStatut(true);
+      }
+    });
+  }
+
+  process(){
+
+    this.okButton.addEventListener('click', () =>{
+      
+      if(this.difficultElem.value == 'easy'){
+        if(this.chanceElem.value > 0){
+          if(this.randomNum == this.input.value){
+            this.chanceElem.value = 0;
+            this.message();
+          }else if(this.randomNum < this.input.value || this.randomNum > this.input.value){
+            --this.chanceElem.value;
+            this.looseLife();
+            this.message();
+          }
+        }
+      }else if(this.difficultElem.value == 'hard'){
+        if(this.chanceElem.value > 0){
+          if(this.randomNum == this.input.value){
+            this.chanceElem.value = 0;
+            this.message();
+          }else if(this.randomNum < this.input.value || this.randomNum > this.input.value){
+            --this.chanceElem.value;
+            this.looseLife();
+            this.message();
+            this.randomNum = Math.floor(Math.random()*11);
+            // console.log(`Après reset: ${this.randomNum}`); 
+          }
+        }
+      }
+      
+    });    
+    
   }
   
 }
 
 
-class Game extends Display{
 
-  // randomNb
-  // Difficulty
-  // nbOfLife
-  // functionality
-  // enableEnterButton
-  // enableOk
-  // reset
-
-  
-}
+let displayElem = new Display();
+displayElem.init();
 
 
-
-let displayElem = new Display('easy');
 let process = new Game();
-
-
-
-
+process.init();
